8719b5d6654ac7b3f8acae5349bff0d4ad30d834
a1GoXplorer <Mark_Daniels1982@hotmail.com>
2014-11-19 18:24:48 -0800

    add isPrime to Functions.js

--- Functions.js
+++ Functions.js
@@ -13,3 +13,81 @@ var count = 0;
   }
 
   console.log(sillySum(myArray));
+
+
+
+
+/*
+ var numSquare = function(max) {
+
+   var maxVal = 10;
+   var dumpArr = [];
+
+  for(var i = 0, i < max, i++)
+    {
+
+
+    }
+
+ }
+
+ console.log(numSquare(maxVal));
+
+ */
+
+var primeOrNot = 17;
+
+ var isPrime = function(n)
+
+{
+	if (n<=3)
+	{
+		return n > 1;
+	}
+
+    if (n % 2 === 0 || n % 3 === 0)
+    {
+    	return false;
+    }
+
+    for (var i = 5; i * i <= n; i += 6)
+    {
+
+      if (n % i === 0 || n & (i + 2) == 0)
+      {
+
+          return false;
+      
+      }
+
+      
+    }
+   return true
+}
+
+console.log(isPrime(primeOrNot));
+
+
+/*
+
+function isPrime(n) {
+
+    if (n <= 3) {
+     return n > 1; 
+ }
+    if (n % 2 == 0 || n % 3 == 0) {
+     return false; 
+ }
+    for (var  i = 5; i * i <= n; i += 6) {
+
+        if (n % i == 0 || n % (i + 2) == 0) 
+        	{
+        	 return false; 
+        	}
+    }
+    return true;
+}
+
+*/
+
+
