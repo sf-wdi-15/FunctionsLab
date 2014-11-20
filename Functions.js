1f9b314bc69a69d87a30a7ee6b53f9dd01be43e4
a1GoXplorer <Mark_Daniels1982@hotmail.com>
2014-11-20 06:59:23 -0800

    add lettercount and merge

--- Functions.js
+++ Functions.js
@@ -1,5 +1,7 @@
 
 
+//sillySum
+
 var myArray = [1,2,3,4,5,6,7,8,9]
 
 var sillySum = function(myArray) {
@@ -14,7 +16,7 @@ var count = 0;
 
   console.log(sillySum(myArray));
 
-
+//Pain in my ass numSquare which Calvin helped with (Thank you, Calvin!)
 
  var maxVal = 10
  var dumpArr = [];
@@ -30,7 +32,7 @@ var count = 0;
 console.log(numSquare(maxVal));
 console.log(dumpArr);
 
-
+//Primality test
 
 var primeOrNot = 17;
 
@@ -65,25 +67,69 @@ var primeOrNot = 17;
 console.log(isPrime(primeOrNot));
 
 
+//Optimus Prime
+
+// function isPrime(n) {
+
+//     if (n <= 3) {
+//      return n > 1; 
+//  }
+//     if (n % 2 == 0 || n % 3 == 0) {
+//      return false; 
+//  }
+//     for (var  i = 5; i * i <= n; i += 6) {
+
+//         if (n % i == 0 || n % (i + 2) == 0) 
+//         	{
+//         	 return false; 
+//         	}
+//     }
+//     return true;
+// }
 
 
-function isPrime(n) {
+//letter count
 
-    if (n <= 3) {
-     return n > 1; 
- }
-    if (n % 2 == 0 || n % 3 == 0) {
-     return false; 
- }
-    for (var  i = 5; i * i <= n; i += 6) {
+var word = "mississippi";
 
-        if (n % i == 0 || n % (i + 2) == 0) 
-        	{
-        	 return false; 
-        	}
+var letterCount = function(letters) {
+
+  var result = {}
+
+  for(var i = 0; i < letters.length; i++) {
+
+    if (result[letters[i]]) {
+      result[letters[i]] += 1;
+    } else {
+      result[letters[i]] = 1;
     }
-    return true;
+
+  }
+
+  return result;
 }
+
+console.log(letterCount(word));
+
+
+//merge
+
+
+var list2 = [3,6,11];
+var list1 = [2,4,5,8,9];
+
+var merge = function(arr1, arr2) {
+  var result = [], next;
+
+  while (arr1.length && arr2.length) {
+    result.push(( arr1[0] <= arr2[0] ) ? arr1.shift() : arr2.shift());
+  }
+  return result.concat(arr1).concat(arr2);
+}
+
+console.log(merge(list1, list2));
+
+
 
 
 
